c8b0ff4c9503acaab67d52cfe45ecb274dc72cae
Maksim Stefanenko <maksim.stefanenko@welmed.it>
2021-10-13 14:13:58 +0300

    Add unit test

---
+++ src/components/FormSubmitStatus/index.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import '@testing-library/jest-dom';
+
+import FormSubmitStatus from '.';
+
+describe('FormSubmitSection', () => {
+  test('should be success', () => {
+    render(<FormSubmitStatus status="success" />);
+
+    expect(screen.getByText(/Success!/i).parentElement).toHaveClass('makeStyles-success-4');
+  });
+
+  test('should be error', () => {
+    render(<FormSubmitStatus status="error" />);
+
+    expect(screen.getByText(/error/i).parentElement).toHaveClass('makeStyles-error-7');
+  });
+
+  test('should be loading', () => {
+    render(<FormSubmitStatus status="loading" />);
+
+    expect(screen.getByText(/sending/i).parentElement).toHaveClass('makeStyles-loading-13');
+  });
+
+  test('should be null', () => {
+    const { container } = render(<FormSubmitStatus status="idle" />);
+
+    expect(container.firstChild).toBe(null);
+  });
+});
