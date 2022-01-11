01ca7d75e1a89d85efa90f7bd7260ad21f12dc7a
Maksim Stefanenko <m.stefanenko@itransition.com>
2022-01-12 00:28:18 +0300

    Fix vendors build

--- src/vendors.js
+++ src/vendors.js
@@ -1,10 +1,7 @@
 /* eslint-disable react/no-typos */
 import 'react';
 import 'react-dom';
-import 'react-redux';
 import 'react-router';
-import 'redux';
-import 'redux-thunk';
 import 'core-js';
 import 'react-router-dom';
 import 'regenerator-runtime';
